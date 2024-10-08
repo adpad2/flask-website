e3b2ff15fdb1c16edbd98adb348a0624e51f2cb7
Adarsh Padalia <adarshkpadalia@gmail.com>
2024-10-08 08:15:04 -0400

    Removed weight-only state loading for pythonanywhere compatibility

--- api/init.py
+++ api/init.py
@@ -3,6 +3,6 @@ import torch
 
 def init_generator(generator_path):
     netG = Generator(32, 256, 16)
-    netG.load_state_dict(torch.load(generator_path, weights_only=True))
+    netG.load_state_dict(torch.load(generator_path))
     netG.eval()
     return netG
